94d86d5211703122a1715f3f7ca26ab6309a92b9
Odecam1 <timotheedanymacedo@gmail.com>
2024-12-10 15:02:00 +0100

    change cn in lib

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,4 @@
-import { clsx, type ClassValue } from "clsx"
+import { clsx, type ClassArray } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...input: ClassArray) => twMerge(clsx(input))
--- src/utils/cn.ts
+++
@@ -1,4 +0,0 @@
-import { ClassArray, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export const cn = (...input: ClassArray) => twMerge(clsx(input))
